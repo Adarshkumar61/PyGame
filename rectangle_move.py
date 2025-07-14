0754f096daa2fbec0f5a159df91b461c83d9ae48
Adarshkumar61 <adarshb2k61@gmail.com>
2025-07-14 09:20:54 +0530

    updated

--- rectangle_move.py
+++ rectangle_move.py
@@ -1,4 +1,5 @@
 import pygame as py
+import numpy as np
 import sys as s
 py.init()
 
@@ -6,3 +7,9 @@ width, height = 800, 600
 screen = py.display.set_mode((width, height))
 py.display.set_caption('Rectangle move with arrow')
 
+#player info:
+player_x = np.random.randint(30, 500)
+player_y = np.random.randint(30, 500)
+player_width = 50
+player_height = 50
+player_movement = 5
